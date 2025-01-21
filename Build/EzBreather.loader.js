455e2feb1898bb2ce4525badc98aa84f436f65f1
Jonnyblue <jonathanvenn@gmail.com>
2025-01-22 06:55:30 +0800

    Report Hours Fixed

--- Build/EzBreather.loader.js
+++ Build/EzBreather.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 69330869,
+    wasmFileSize: 69330907,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
