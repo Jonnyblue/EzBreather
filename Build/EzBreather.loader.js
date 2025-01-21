47935062b14618c8d539cff43383bffc43775cbc
Jonnyblue <jonathanvenn@gmail.com>
2025-01-22 05:41:32 +0800

    Saving?

--- Build/EzBreather.loader.js
+++ Build/EzBreather.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 56709048,
+    wasmFileSize: 56709090,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
